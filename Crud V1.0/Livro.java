4b17b119e08e277da986e8d9be17d5ca555d36d2
Lucas Santiago <lu.santi.oli@gmail.com>
2020-08-28 18:48:26 -0300

    Livro implementa Registro

--- Crud V1.0/Livro.java
+++ Crud V1.0/Livro.java
@@ -1,7 +1,15 @@
-public class Livro {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+
+public class Livro implements Registro {
     private String nomeLivro;
     private String nomeAutor;
-    private float preco;
+    private float  preco;
+    private int    id; 
 
     // Construtor de um livro vazio
     public Livro() {
@@ -13,6 +21,31 @@ public class Livro {
         this.nomeLivro = nome;
         this.nomeAutor = autor;
         this.preco     = preco;
+    }
+
+    // Set ID
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Set nome do Livro
+    public void setNome(String nome) {
+        this.nomeLivro = nome;
+    }
+
+    // Set nome do Autor
+    public void setAutor(String autor) {
+        this.nomeAutor = autor;
+    }
+
+    // Set preco do Livro
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    // Get ID
+    public int getId() {
+        return this.id;
     }
 
     // Get nome do Livro
@@ -28,5 +61,29 @@ public class Livro {
     // Get nome do Livro
     public float getPreco() {
         return this.preco;
+    }
+
+    // Serializar objeto
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        DataOutputStream      data      = new DataOutputStream(byteArray);
+        
+        data.writeInt(id);
+        data.writeFloat(preco);
+        data.writeUTF(nomeLivro);
+        data.writeUTF(nomeLivro);
+        
+        return byteArray.toByteArray();
+    }
+
+    // Desserializar objeto
+    public void fromByteArray(byte[] arrayObjeto) throws IOException {
+        ByteArrayInputStream byteArray = new ByteArrayInputStream(arrayObjeto);
+        DataInputStream      data      = new DataInputStream(byteArray);
+
+        this.id        = data.readInt();
+        this.preco     = data.readFloat();
+        this.nomeLivro = data.readUTF();
+        this.nomeAutor = data.readUTF();
     }
 }
