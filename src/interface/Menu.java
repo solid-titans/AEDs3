3e93ace33a7628a70e637ddd92b4ed11e101a805
MysteRys337 <mysteriousfunkyman@gmail.com>
2020-09-18 20:26:14 -0300

    Atualizado o menu - novas funcionalidades

--- src/interface/Menu.java
+++ src/interface/Menu.java
@@ -4,19 +4,63 @@ import java.io.*;
 
 public class Menu {
 
-    public static ASCIInterface a = new ASCIInterface(); // Interface grafica feita em ASCII
+    //Id do Usuario que usar o sistema
+    public static int IdUsuario             = - 1;
+
+    public static ASCIInterface a           = new ASCIInterface(); // Interface grafica feita em ASCII
 
     //Variaveis para leitura do teclado
-    public static InputStream is = System.in;
-    public static InputStreamReader isr = new InputStreamReader(is);
-    public static BufferedReader br = new BufferedReader(isr);
+    public static InputStream is            = System.in;
+    public static InputStreamReader isr     = new InputStreamReader(is);
+    public static BufferedReader br         = new BufferedReader(isr);
+
+    //String com as interfaces dos diferentes menu
+
+    //Menu 0 : Tela de Acesso
+    public static String acesso() {
+        return "ACESSO\n\n" +
+               "1) Acesso ao sistema\n" +
+               "2) Novo usuario\n" +
+               "3) Esqueci minha senha\n\n"+
+               "0) Sair\n\n" + 
+               "Opção: ";
+    }
+
+    //Menu 1 : Tela de Inicio
+    public static String inicio(byte notificacoes) {
+        return "INÍCIO\n\n" +
+               "1) Criação de perguntas\n" +
+               "2) Consultar/responder perguntas\n" +
+               "3) Notificações: " + notificacoes + "\n\n"+
+               "0) Sair\n\n" + 
+               "Opção: ";
+    }
+
+    //Menu 2 : Tela de criacaoDePerguntas
+    public static String criacaoDePerguntas() {
+        return "INÍCIO > CRIAÇÃO DE PERGUNTAS\n\n" +
+               "1) Listar\n" +
+               "2) Incluir\n" +
+               "3) Alterar\n"+
+               "4) Arquivar\n\n" + 
+               "0) Retornar ao menu anterior\n\n"+
+               "Opção: ";
+    }
+
+    //Menu 21 : Tela de listagem de perguntas
+    public static String listagem() {
+        return "Minhas perguntas\n\n"+
+               "Pressione qualquer tecla para continuar...";
+    }
 
     public static void main(String[] args) {
 
         //Variaveis do menu
         String opcao;
-        short menuIndex = 0;
-        short notificacoes = 0;
+        byte menuIndex = 0;
+        byte notificacoes = 0;
+
+        boolean sucesso = false;
 
         //Loop do menu
         do {
@@ -27,7 +71,7 @@ public class Menu {
             //Imprimir uma caixa com o titulo
             System.out.println(a.caixa((short)5,"PERGUNTAS 1.0"));
 
-            //Identificar qual menu deve ser imprimido
+            //Imprimindo o menu a ser exibido para o usuario
             switch(menuIndex) {
 
                 //Acesso
@@ -71,7 +115,7 @@ public class Menu {
             //limpando a tela
             a.limparTela();
 
-            System.out.println(opcao);
+            //System.out.println(opcao);
 
             //Fazendo a mudança do menu
             /*
@@ -98,17 +142,41 @@ public class Menu {
                     //Menu Acesso
     
                     case "00": //Saindo do programa
-                        System.out.println("Obrigado por usar o programa!\nTenha um excelente dia");
+                        System.out.println("Obrigado por usar o programa!\nTenha um excelente dia!");
                         break;
+
                     case "10": // Acessando o sistema usando credenciais existentes
-                        System.out.println("Acesso ao sistema ta ai");
-                        menuIndex = 1;
+                        sucesso = acessoAoSistema();
+
+                        if(sucesso) {
+                            System.out.println("\nSucesso! Login aprovado!\nSeja bem vindo usuário!\n")
+                            menuIndex = 1;
+                        }
+                        else {
+                            System.err.println("\nERRO! Login não aprovado!\nTente novamente!\n");
+                        }
                         break;
                     case "20": // Criando um novo usuario 
-                        System.out.println("Bora criar um novo usuario");
+                        sucesso = criandoUsuario();
+
+                        if(sucesso) {
+                            System.out.println("\nSucesso! Novo usuário criado! Agora faça login!\n")
+                            menuIndex = 1;
+                        }
+                        else {
+                            System.err.println("\nERRO! Criação de usuário deu errado!\nTente novamente!\n");
+                        }
                         break;
+
                     case "30": // Resentando a senha
-                        novaSenha();
+                        sucesso = novaSenha();
+                        if (sucesso) {
+                            
+                            System.out.println("\nSucesso! A senha da sua conta foi alterada!\nVoltando ao menu...");
+                        }
+                        else {
+                            System.out.println("\nERRO! Não foi possível fazer a operação de mudança de senha!\nTente novamente!\n");
+                        }
                         break;
 
                     //Menu Inicio
@@ -116,12 +184,15 @@ public class Menu {
                     case "01": // Saindo do programa
                         System.out.println("Obrigado por usar o programa!\nTenha um excelente dia");
                         break;
+
                     case "11": // Indo para a tela de criacao de perguntas
                         menuIndex = 2;
                         break;
+
                     case "21": // Indo para a tela de consultar/responder perguntas
                         System.out.println("Vamos consultar/responder perguntas");
                         break;
+
                     case "31": // Verificar suas notificacoes
                         System.out.println("Olha so as notificacoes");
                         break;
@@ -131,15 +202,19 @@ public class Menu {
                     case "02": // Voltando a tela de Inicio
                         menuIndex = 1;
                         break;
+
                     case "12": // Listando as perguntas do usuario atual
                         menuIndex = 12;
                         break;
+
                     case "22": // Incluindo uma nova pergunta
                         System.out.println("Vamos Incluir!");
                         break;
+
                     case "32": // Alterando uma pergunta atual
                         System.out.println("Vamos Alterar");
                         break;
+
                     case "42": // Arquivando as perguntas
                         System.out.println("Arquivar");
                         break;
@@ -160,53 +235,15 @@ public class Menu {
 
     }
 
-    //String com as interfaces dos diferentes menu
-
-    //Menu 0 : Tela de Acesso
-    public static String acesso() {
-        return "ACESSO\n\n" +
-               "1) Acesso ao sistema\n" +
-               "2) Novo usuario\n" +
-               "3) Esqueci minha senha\n\n"+
-               "0) Sair\n\n" + 
-               "Opção: ";
-    }
-
-    //Menu 1 : Tela de Inicio
-    public static String inicio(short notificacoes) {
-        return "INÍCIO\n\n" +
-               "1) Criação de perguntas\n" +
-               "2) Consultar/responder perguntas\n" +
-               "3) Notificações: " + notificacoes + "\n\n"+
-               "0) Sair\n\n" + 
-               "Opção: ";
-    }
-
-    //Menu 2 : Tela de criacaoDePerguntas
-    public static String criacaoDePerguntas() {
-        return "INÍCIO > CRIAÇÃO DE PERGUNTAS\n\n" +
-               "1) Listar\n" +
-               "2) Incluir\n" +
-               "3) Alterar\n"+
-               "4) Arquivar\n\n" + 
-               "0) Retornar ao menu anterior\n\n"+
-               "Opção: ";
-    }
-
-    //Menu 21 : Tela de listagem de perguntas
-    public static String listagem() {
-        return "Minhas perguntas\n\n"+
-               "Pressione qualquer tecla para continuar...";
-    }
-
     //Interface 03: Criação de nova senha
-    public static void novaSenha() {
+    public static boolean novaSenha() {
 
         String email          = "";
         String novaSenha      = "";
         String confirmarSenha = "";
 
         boolean senhasIguais  = false;
+        boolean resp = false;
 
         int forca = -1;
 
@@ -231,15 +268,14 @@ public class Menu {
 
                 System.out.println("Nova senha: ");
 
+                //Leitura da senha
+                /*
+                *   O usuario precisa fazer a inserção da senha
+                *   duas vezes para conferir se o mesmo sabe
+                *   qual e a senha
+                */
                 try {
                     novaSenha = br.readLine();
-                }
-                catch(IOException e) {
-                    System.err.println("Erro na leitura do buffer!");
-                }
-                System.out.println("Confirme a nova senha: ");
-
-                try {
                     confirmarSenha = br.readLine();
                 }
                 catch(IOException e) {
@@ -264,14 +300,158 @@ public class Menu {
                                        "*   -> Possuir algum caractere especial(Exemplo: *?#)\n" + 
                                        "*   -> Possuir pelo menos 1 digito\n\n" +
                                        "Obs: Recomendamos no mínimo uma senha de força 3.");
+
                 }
             } while( senhasIguais == false || forca <= 2);
+
+            resp = true;
+
         }
         else {
-            System.err.println("O email que foi inserido não é valido! Tente novamente!");
+            System.err.println("\nO email que foi inserido não é valido\nOu não consta em nosso banco de dados!\n Tente novamente!\n");
         }
-        System.out.println("\nSucesso! a senha da sua conta foi alterada!\nVoltando ao menu...");
+
+        return resp;
+    }
+
+    //Funcao booleana expressando se o usuario tem acesso ou não ao sistema
+    /* 
+    *   Primeiro o usuario tenta inserir o e-mail, se o mesmo consta no banco
+    *   de dados, então ele passa para a verificação de senha, se a senha
+    *   não for válida, então o usuario não ganha acesso.
+    */
+    public static boolean acessoAoSistema() {
+
+        String email = "";
+        String senha = "";
+        boolean resp = false;
+
+        byte tentativas = 5;
         
+        do { 
+            System.out.println(a.caixa((short)5,"Bem vindo usuário!"));
+            System.out.println("ACESSO AO SISTEMA\n\nE-mail: ");
+
+            try {
+                email = br.readLine();
+            }  
+            catch(IOException e) {
+                System.err.println("Erro na leitura do buffer!");
+            }
+
+            a.limparTela();
+            if(verificarEmail(email)) {
+
+                System.out.println(a.caixa((short)5,"Insira sua senha:"));
+                System.out.println("\nACESSO AO SISTEMA\nSenha: ");
+
+                try {
+                    senha = br.readLine();
+                }  
+                catch(IOException e) {
+                    System.err.println("Erro na leitura do buffer!");
+                }
+
+                a.limparTela();
+                if ( 1 != 1 ) {
+                        
+                    System.out.println("Ops! Parece que essa não é a senha!\nTente novamente!\nN° de tentativas: " + tentativas + "\n");
+                }
+                else {
+                        
+                    resp = true;
+                }
+            }
+            else {
+                
+                tentativas--;
+                System.out.println("\nO email inserido não é válido ou não existe no banco de dados\nTeste novamente!\nN° de tentativas: " + tentativas + "\n");
+            }
+        } while( tentativas != 0 );
+
+        
+        return resp;
+    }
+
+    public static boolean criandoUsuario() {
+
+        String email     = "";
+        String usuario   = "";
+        String senha     = "";
+
+        String confirmar = "";
+        boolean resp     = false;
+
+        do {
+            System.out.println(a.caixa((short)5,"Vamos criar um novo usuário!"));
+            System.out.println("NOVO USUÁRIO\n\nEmail: ");
+
+            try {
+                email = br.readLine();
+            }  
+            catch(IOException e) {
+                System.err.println("Erro na leitura do buffer!");
+            }
+            
+            a.limparTela();
+            if ( 1 != 1 ) {
+
+                System.out.println("Esse e-mail já está registrado em nosso sistema\nTente outro e-mail!\n");
+
+            }
+        } while ( 1 != 1);
+    
+        if(verificarEmail(email)) {
+
+            System.out.println(a.caixa((short)5,"Digite um nome e senha!"));
+            System.out.println("NOVO USUÁRIO\n\nUsuário: ");
+
+            try {
+                usuario = br.readLine();
+                a.limparTela();
+                System.out.println(a.caixa((short)5,"Digite um nome e senha!"));
+                System.out.println("NOVO USUÁRIO\n\nSenha: ");
+                usuario = br.readLine();
+            }  
+            catch(IOException e) {
+                System.err.println("Erro na leitura do buffer!");
+            }
+
+            a.limparTela();
+            System.out.println("Vamos então verificar os seus dados!" +
+                               "Email: "           + email     + "\n" + 
+                               "Nome de usuário: " + usuario   + "\n" +
+                               "Senha: "           + senha     + "\n");
+            
+            System.out.println("\nEstá tudo de acordo?");
+
+            try {
+                confirmar = br.readline();
+            }  
+            catch(IOException e) {
+                System.err.println("Erro na leitura do buffer!");
+            }
+
+            a.limparTela();
+            if(confirmar.length() == 0 || confirmar.toLowerCase().equals("s")) { 
+                
+                System.out.println("Certo! Vamos então criar o usuario "+ usuario +"para você!");
+                resp = true;
+                System.out.println("Sucesso! Usuário criado! Voltando ao menu!\n")
+            }
+            else {
+
+                System.out.println("Processo cancelado!\nVoltando para o menu...\n");
+            }
+
+            
+        }
+        else {
+            System.err.println("O email inserido é inválido!\nVoltando ao menu...\n\n");
+        }
+
+        return resp;
+
     }
 
 
@@ -328,7 +508,7 @@ public class Menu {
     	    if(!s.matches(".*\\d.*")) {
 
     	        forca--;
-              System.out.println("Tente inserir pelo menos um numero na sua senha\n");
+                System.out.println("Tente inserir pelo menos um numero na sua senha\n");
             }
 	    }
       //Se a String estiver vazia
