59eed783fef2fcfaa3aedc3ef8acacc8954bc40b
MysteRys337 <mysteriousfunkyman@gmail.com>
2020-10-02 22:57:41 -0300

    Versão 0.0.2 da ASCIInterface. Agora é possível inserir textos longos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 import menu.Menu;
-import menu.sistema.graficos.*;
 
 public class Main
 {
     public static void main(String[] args) {
-        //Menu m = new Menu();
-        //m.Inicio();
-        ASCIInterface a = new ASCIInterface();
-        a.setBorda(8);
-        a.setJanela(69);
-        System.out.println(a.caixa(5,"Se liga ai Leonel"));
+        Menu m = new Menu();
+        m.Inicio();
     }
 
 }
--- src/menu/sistema/graficos/ASCIInterface.java
+++ src/menu/sistema/graficos/ASCIInterface.java
@@ -8,9 +8,16 @@ package menu.sistema.graficos;
 
 public class ASCIInterface {
 
+
+    private final StringManipulation s = new StringManipulation();
     //Definindo a altura e largura maxima dos elementos
-    private final short ALTURA_MAX = 1000;
-    private final short LARGURA_MAX = 1000;
+    private final short ALTURA_MAX       = 1000;
+    private final short LARGURA_MAX      = 1000;
+
+    private final byte LARGURA_SEGURANCA = 5;
+    private final byte ALTURA_SEGURANCA  = 4;
+
+    private final byte LARGURA_PADRAO    = 30;
 
     //Declarando os elementos que guardam as informações de texto
     /*
@@ -37,24 +44,12 @@ public class ASCIInterface {
     }
 
     //Funções 'set'
-    public void setBorda(int cor ) {
-        if ( cor > 0 && cor < 256) {
-            setBorda((short)cor);
-        }
-    }
-
-    private void setBorda(short cor) {
+    public void setBorda(int cor) {
         borda.setCor(cor);
         borda.setFundo(cor);
     }
 
     public void setJanela(int cor) {
-        if ( cor > 0 && cor < 256) {
-            setJanela((short)cor);
-        }
-    }
-
-    private void setJanela(short cor) {
         janela.setCor(cor);
         janela.setFundo(cor);
         texto_primario.setFundo(cor);
@@ -62,28 +57,15 @@ public class ASCIInterface {
     }
 
     public void setTextoPrimario(int cor) {
-        if ( cor > 0 && cor < 256) {
-            setTextoPrimario((short)cor);
-        }
-    }
-
-    private void setTextoPrimario(short cor) {
         texto_primario.setCor(cor);
-        texto_primario.setFundo(cor);
     }
 
-    public void setTextoSecundario(int cor) {
-        if ( cor > 0 && cor < 256) {
-            setTextoSecundario((short)cor);
-        }
-    }
-
-    private void setTextoSecundario(short cor) {
+    public void setTextoSecundario(short cor) {
         texto_secundario.setCor(cor);
     }
 
     //Instanciando uma caixa
-    /*
+    /** 
     *   A caixa é um espaço retangular ou quadricular
     *   Onde tem uma borda e o um único texto.
     *
@@ -94,6 +76,9 @@ public class ASCIInterface {
     *
     *  -> A que recebe apenas a altura e String, a largura
     *   é inferida como sendo a largura da String.
+    *
+    *  -> A que recebe apenas o String, a largura e altura
+    *   é tomado como referência a String
     *   
     *   OBS: Por enquanto, nos dois casos, a String estará
     *   centralizada na caixa.
@@ -104,38 +89,99 @@ public class ASCIInterface {
 
         String resp    = "";
         String[] array = null;
-        
-        if(texto.length() > largura ) {
-            array = getStringArray(texto,largura);
-        }
-        else if ( texto.length() < largura && encaixa(largura,altura) ) {
-            resp = caixa((short)largura,(short)altura,texto);
+        largura += LARGURA_SEGURANCA + 1;
+
+        if(texto.length() > largura) {
+
+            array = texto.split(" ");
+            altura  += ALTURA_SEGURANCA;
+            
+            if(encaixa(largura,altura) && s.calcularTamanho(array, largura) < altura) {
+                resp = caixa((short)largura,(short)altura,array);
+            }
         }
         else {
-            System.err.println("ERRO! O texto enviado é maior do que a caixa");
+            if ( texto.length() < largura && encaixa(largura,altura) ) {
+                resp = caixa((short)largura,(short)altura,texto);
+            }
+            else {
+                resp = texto;
+            }
         }
-
         return resp;
     }
 
-    //Caixa 2: altura e texto(a largura é inferida como sendo o tamanho de 'texto')
+    //Caixa 2: altura e texto(a largura é inferida como sendo o tamanho de 'texto' + a largura de seguranca)
     public String caixa(int altura, String texto) {
 
-        String resp = "";
-        int largura = texto.length() + 4;
-        if (encaixa(largura,altura) ) {
-            resp = caixa((short)largura,(short)altura,texto);
+        String resp    = "";
+        int largura    = -1;
+        String[] array = null;
+
+        if(texto.length() > LARGURA_PADRAO) {
+            array    = texto.split(" ");
+            largura  = LARGURA_PADRAO + LARGURA_SEGURANCA + 1;
+            altura  += ALTURA_SEGURANCA;
+            
+            if(encaixa(largura,altura) && s.calcularTamanho(array, largura) < altura) {
+                resp = caixa((short)largura,(short)altura,array);
+            }
         }
-        else {
-            System.err.println("ERRO! O texto enviado é maior do que a caixa");
+        else { 
+            largura = texto.length() + LARGURA_SEGURANCA;
+            if (encaixa(largura,altura) ) {
+                resp = caixa((short)largura,(short)altura,texto);
+            }
+            else {
+                System.err.println("ERRO! O texto enviado é maior do que a caixa");
+            }
         }
 
         return resp;
     }
+
+    //Caixa 3: apenas texto(a largura é inferida como sendo o tamanho de 'texto' + a largura de seguranca)
+    //                      e a altura é inferida como sendo a altura de seguranca)
+    public String caixa(String texto) {
+
+        String   resp  = "";
+        String[] array = null;
+        int largura = -1;
+        int altura  = -1;
+
+
+        if(texto.length() > LARGURA_PADRAO) {
+            array = texto.split(" ");
+            largura = LARGURA_PADRAO + LARGURA_SEGURANCA + 1;
+            altura  = s.calcularTamanho(array, largura) + ALTURA_SEGURANCA;
+            
+            if(encaixa(largura,altura)) {
+                resp = caixa((short)largura,(short)altura,array);
+            }
+        }
+        else { 
+            largura = texto.length() + LARGURA_SEGURANCA + 1;
+            altura = ALTURA_SEGURANCA + 1;
+
+            if (encaixa(largura,altura) ) {
+                resp = caixa((short)largura,(short)altura,texto);
+            }
+            else {
+                resp = texto;
+            }
+        }
+
+        return resp;
+    }
+
+    //Funções para Imprimir uma caixa
+    /**
+     *  @ recebem uma largura, altura e um texto para formar uma caixa
+     *  @ gera uma String que contém caixa
+     */
     
     //Função para criar a caixa efetivamente e manda-la devolta como String
     private String caixa(short largura, short altura, String texto) {
-
         String caixa = "";
         boolean textoInserido = false;
 
@@ -146,10 +192,39 @@ public class ASCIInterface {
                     caixa += borda.imprimir("■");
                 }
                 else {
-                    if ( !textoInserido && i == altura/2 && j >= largura - texto.length() - j) {
+                    if ( !textoInserido && i == altura/2 && j > largura - texto.length() - j - 1) {
                         caixa += texto_primario.imprimir(texto);
-                        j += texto.length() -1 ;
+                        j += texto.length() - 1;
                         textoInserido = true;
+                    }
+                    else {
+                        caixa += janela.imprimir("▒");
+                    }
+                }
+            }
+            caixa += "\n";
+         }
+        return caixa;
+    }
+
+    //Função para imprimir a caixa de texto efetivamente e manda-la devolta como String
+    private String caixa(short largura,short altura,String[] texto) {
+
+        String caixa = "";
+        byte textoIndex = 0;
+
+        for (short i = 0 ; i < altura; i++) {
+            for (short j = 0; j < largura; j++) {
+
+                if(i == 0 || i == altura -1 || j == 0 || j == largura -1) {
+                    caixa += borda.imprimir("■");
+                }
+                else {
+                    if ( textoIndex < texto.length  && i > 1 && j > 1 && j + texto[textoIndex].length() < largura ) {
+                        texto[textoIndex] += " ";
+                        caixa += texto_primario.imprimir(texto[textoIndex]);
+                        j += texto[textoIndex].length() -1 ;
+                        textoIndex++;
                     }
                     else {
                         caixa += janela.imprimir("▒");
@@ -187,28 +262,13 @@ public class ASCIInterface {
                 Runtime.getRuntime().exec("cls");
             }
             catch (Exception e) {
-                System.out.println("Deu ruim");
+                System.err.println("Deu ruim");
             }
         }
     }
 
     //Função que returna 'true' ou false, se as medidas da caixa são validas
     private boolean encaixa(int largura, int altura) {
-
         return (largura > 0 && altura > 2 && largura < LARGURA_MAX && altura < ALTURA_MAX);
-    }
-
-    private String[] getStringArray(String entrada,int largura) {
-
-        String[] resp = null;
-        String[] aux  = null;
-        if (entrada.contains("\n")) {
-            resp = entrada.split("\n");
-        }
-        else {
-
-        }
-
-        return resp;
     }
 }
